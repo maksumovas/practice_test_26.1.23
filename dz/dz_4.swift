6dcac8946954acb29cc378eb98f7aa9b63bd9521
maksumovas <maksumovasofia@gmail.com>
2022-11-27 23:12:13 +0600

    dz_4

--- dz/dz_4.swift
+++ dz/dz_4.swift
@@ -5,4 +5,62 @@
 //  Created by Maksumova Sofia on 27/11/22.
 //
 
+
+//
+//  main.swift
+//  hw_4_27.11.22
+//
+//  Created by Maksumova Sofia on 27/11/22.
+//
+
 import Foundation
+
+func dz4_1(){
+    var num = 0
+    var out_words: String = ""
+    var cw: String = ""
+    let words = "tv, tea, walk, table, people, journey, mountain, strengths, conscience, performance, announcments, objectivizing, accidentalness, methoxybenzenes, ACCLIMATIZATIONS, administratorship, ABSENTMINDEDNESSES, ABDOMINOHYSTEROTOMY, ABDOMINOHYSTERECTOMY, ANARCHOINDIVIDUALIST"
+
+    for ch in words {
+        if(ch != " "){
+            
+            
+            if(ch == ","){
+            out_words += ("\(cw) - \(num) symbols ")
+                num = 0
+                cw = ""
+            }
+            else {
+                cw += String(ch)
+                num += 1
+            }
+        }
+
+    }
+
+    print(out_words)
+}
+
+func dz4_2(){
+
+    var digits: String = ""
+
+    for i in 1...100{
+        if i % 2 == 0{
+            digits = digits + "," + String(i)
+        }
+        else if i % 3 == 0{
+            digits = digits + "," + String(i)
+        }
+        else if i % 4 == 0{
+            digits = digits + "," + String(i)
+        }
+        else if i % 5 == 0{
+            digits = digits + "," + String(i)
+        }
+    }
+
+    print(digits)
+    
+}
+
